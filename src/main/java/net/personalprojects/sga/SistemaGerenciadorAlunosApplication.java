5387b3661b5e00147d5de16c80e2665c7910199b
Alarju <alex.geass@gmail.com>
2022-03-31 15:09:22 -0300

    configurando MySQL

---
+++ src/main/java/net/personalprojects/sga/SistemaGerenciadorAlunosApplication.java
@@ -0,0 +1,13 @@
+package net.personalprojects.sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciadorAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/personalprojects/sga/SistemaGerenciadorAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.personalprojects.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
